e1448c0b82e666ee0ec49431c31eab509c57f7f7
IlyaManishin <manishin06@gmail.com>
2026-01-27 23:21:41 +0700

    fix scaling

--- board/prices_printer/fb_helper.py
+++ board/prices_printer/fb_helper.py
@@ -8,21 +8,25 @@ def get_bytearr_size(w, h):
 
 
 def draw_text_scaled(fb, text, x, y, color, scale, max_x):
-    w = int(CH_SZ_X * scale)
-    h = int(CH_SZ_Y * scale)
-    glyph_buf = bytearray(get_bytearr_size(w, h))
-    ch_temp = framebuf.FrameBuffer(glyph_buf, w, h, framebuf.MONO_HLSB)
+    is_compressed = (scale <= 1)
 
+    glyph_buf = bytearray(get_bytearr_size(CH_SZ_X, CH_SZ_Y))
+    ch_temp = framebuf.FrameBuffer(glyph_buf, CH_SZ_X, CH_SZ_Y, framebuf.MONO_HLSB)
+
+    h = int(round(CH_SZ_Y * scale))
+    w = int(round(CH_SZ_X * scale))
     cx = x
     cy = y
-
     for ch in text:
-        ch_temp.fill(0)
-        ch_temp.text(ch, 0, 0, 1)
+        ch_temp.fill(1)
+        ch_temp.text(ch, 0, 0, 0)
 
-        for py in range(h):
-            for px in range(w):
-                if ch_temp.pixel(px, py):
+        if is_compressed:
+            for px in range(CH_SZ_X):
+                for py in range(CH_SZ_Y):
+                    if ch_temp.pixel(px, py) == 1:
+                        continue
+                    
                     px_scaled = int(round(px * scale))
                     py_scaled = int(round(py * scale))
                     size_scaled = max(1, int(round(scale)))
@@ -34,6 +38,24 @@ def draw_text_scaled(fb, text, x, y, color, scale, max_x):
                         size_scaled,
                         color
                     )
+        else:
+            for px in range(w):
+                for py in range(h):
+                    px_base = int(round(px / scale))
+                    py_base = int(round(py / scale))
+                    if px_base >= CH_SZ_X or py_base >= CH_SZ_Y:
+                        continue
+                    if ch_temp.pixel(px_base, py_base) == 1:
+                        continue
+                    
+                    fb.fill_rect(
+                        cx + px,
+                        cy + py,
+                        1,
+                        1,
+                        color
+                    )
+
         cx += w
         if cx + w > max_x:
             break
